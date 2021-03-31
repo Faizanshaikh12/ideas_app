3494d5fcd8b4f735eeb9e444fb145ed29188cd0d
Faizanshaikh12 <faizanroy000777@gmail.com>
2021-03-31 18:23:02 +0530

    main.ts minor changes (cors error)

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ const port = process.env.PORT || 6000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:4200', // angular
+    ],
+  });
   await app.listen(port);
 
   Logger.log(`Server running on ${port}`, 'Bootstrap');
